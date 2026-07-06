b366736408329be50b54540a832a48c68ea5f94d
yoke <1369305176@qq.com>
2026-07-07 00:23:55 +0800

    chore(release): bump version to 1.0.2 and update changelog

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -38,7 +38,7 @@ android {
         applicationId = "com.yoke.gainful"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 2
+        versionCode = 3
         versionName = property("VERSION_NAME").toString()
     }
     packaging {
